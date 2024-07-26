23304037773f121b13c04133367e288de2d50a96
Sachin <s474996633@gmail.com>
2024-07-26 14:53:48 +0530

    random themes

--- backend/src/controllers/whatsapp/webhook.controller.ts
+++ backend/src/controllers/whatsapp/webhook.controller.ts
@@ -1,14 +1,19 @@
 import AbstractController from "controllers/index.controller";
 import { InternalServerError } from "errors/internal-server-error";
 import { NextFunction, Request, Response } from "express";
+import z from 'zod';
 
 class WhatsAppController extends AbstractController {
     post() {
+        const payloadSchema = z.object({ notification: z.any() });
+        type IPayload = z.infer<typeof payloadSchema>;
+
         return [
-            async (req: Request, res: Response, next: NextFunction) => {
+            async (req: Request<unknown, unknown, IPayload>, res: Response, next: NextFunction) => {
                 try {
-                    console.log(req);
-                    console.log(res);
+                    const { notification } = req.body;
+                    console.log(notification);
+                    res.sendStatus(200);
                 } catch (e: unknown) {
                     console.error(e);
                     next(new InternalServerError());
